c9db6c3eadac0ee1551051be7820cd3a036616da
jessenboone <jessenboone@gmail.com>
2017-03-21 16:09:59 -0600

    3/21/17

---
+++ A.A.Notes/The DOM-JQuery.js
@@ -0,0 +1,63 @@
+// Use Source.html, jquery-3.1.1, and source.js to link your accounts in the fututre
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width-device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,">
+    <meta http-equiv="X-UA-Compatible" content-"ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+    <div id="content"></div>
+    <div class="contentClass"></div>
+    <p>Hello</p>
+    <script src=""./jquery-3.1.1"></script>
+    <script src="Source.js"></script>
+  </body>
+</html>
+
+Linking JQuery to JavaScript, CSS, and html.
+
+
+
+
+
+
+
+// Ex
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width-device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,">
+    <meta http-equiv="X-UA-Compatible" content-"ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="Source.css">
+
+  </head>
+  <body>
+    <div id="content"></div>
+    <div class="contentClass"></div>
+
+    <input id="firstname" type="text" value="this is an actual val"></input>
+    <input id="" type="button" value="Submit"></input>
+
+    <script src="./jquery-3.1.1 (2).js"></script>
+    <script src="Source.js"></script>
+  </body>
+</html>
+
+// Ex
+
+// Ex
+
+// Ex
+
+// Ex
+
+// Ex
