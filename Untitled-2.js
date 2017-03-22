d5eee84ec29ecfc0777fdad87d9250006ea89923
jessenboone <jessenboone@gmail.com>
2017-03-22 10:58:26 -0600

    'commit these files

---
+++ Untitled-2.js
@@ -0,0 +1,29 @@
+function registerTeam(name) {
+
+    var players = [];
+
+    function addPlayer(playerName) {
+
+
+        players.push(playerName);
+        players;
+        name;
+    }
+    return addPlayer;
+}
+
+var griffonsdoorAddPlayerFn = registerTeam("griffonsdoor");
+
+
+
+
+griffonsdoorAddPlayerFn('Jesse')
+griffonsdoorAddPlayerFn('Kevin')
+griffonsdoorAddPlayerFn('Jake')
+
+
+var SlitherInFn = registerTeam('SlitherIn');
+
+SlitherInFn('kaleigh');
+SlitherInFn('Nick');
+SlitherInFn('Draco');
